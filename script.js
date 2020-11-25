1afb61ec350ff2aa2f35a0bd7016b2e7f9cfe538
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-11-25 09:16:18 +0100

    exo 53

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let introductionElement = document.getElementById("texte");
+
+introductionElement.style.fontWeight = "bold";
+console.log(introductionElement);
+
+introductionElement.style.color = "red";
+
+let animation = document.getElementById("anime");
+animation.style.animation = "action";
+animation.style.animationDuration ="5s";
+
+let elements = document.querySelector("#test");
+elements.style.backgroundColor = "blue";
+console.log(elements);
+
+let texteTaille = document.getElementById("taille");
+texteTaille.style.fontSize = "150%";
+console.log(texteTaille);
+
+let lien = document.getElementById("cible");
+lien.innerHTML = "mon lien";
+
+let direction = document.getElementById("test3");
+direction.dir = "rtl";
+console.log(direction);
+
+let modif = document.getElementById("modif");
+modif.style.color = "green"
+
